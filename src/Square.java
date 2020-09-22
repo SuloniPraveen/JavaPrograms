c673c09e0ba18002f9b280ffa1e3846ed6435361
SuloniPraveen <sulonipraveen@gmail.com>
2020-09-22 22:16:38 +0530

    CLASS 2

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,21 @@
 public class Square {
+    int add_sq(int a,int b)
+    {
+        return (a+b)*(a+b);
+    }
+    int add_sq(int a,int b,int c)
+    {
+        return (a+b+c)*(a+b+c);
+    }
+    int add_sq(int a,int b,int c,int d)
+    {
+        return (a+b+c+d)*(a+b+c+d);
+    }
+    public static void main(String[] args)
+    {
+      Square obj = new Square();
+      System.out.println("Square of sum of 2 no.s: "+obj.add_sq(10,20));
+      System.out.println("Square of sum of 3 no.s: "+obj.add_sq(10,20,30));
+      System.out.println("Square of sum of 4 no.s: "+obj.add_sq(10,20,30,40));
+    }
 }
