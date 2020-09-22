ca5c64ba183bc8565f5b17a8bcac951395658ba1
SuloniPraveen <sulonipraveen@gmail.com>
2020-09-22 23:09:23 +0530

    CLASS_const_overloading

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,21 @@
 class Student {
     String name;
-    public Student(String s){
+    int roll_no;
+    public Student(String s,int r){
         name = s;
+        roll_no = r;
     }
     public Student() {
         name = "Unknown";
+
     }
 }
+
 class Ans{
     public static void main(String[] args){
-        Student s = new Student("Rita");
+        Student s = new Student("Rita",20);
         Student a = new Student();
-        System.out.println(s.name);
+        System.out.println(s.name+" "+s.roll_no);
         System.out.println(a.name);
     }
 }
